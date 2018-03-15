d5726f7a2a576aca353c1f0c2a84aafa82cddb2b
mattheusbarroos <matheusbarros1305@gmail.com>
2018-03-15 01:16:53 -0400

    Calculations are done automatically - no button needed

--- app/src/main/java/com/example/mbarr/discountapp/MainActivity.java
+++ app/src/main/java/com/example/mbarr/discountapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +16,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText originalPriceET;
+    private EditText discountPercentageET;
+    private TextView finalPriceTV;
+
+    private double originalPrice = 0.0;
+    private double discountPercentage = 0.0;
+    private double finalPrice = 0.0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,21 +31,43 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button calculateBtn =  findViewById(R.id.calculateBT);
-        calculateBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                EditText originalPriceET =  findViewById(R.id.originalPriceET);
-                EditText discountPercentageET =  findViewById(R.id.percentageOffET);
-                TextView finalPriceTV =  findViewById(R.id.finalPriceTV);
+        originalPriceET = findViewById(R.id.originalPriceET);
+        discountPercentageET = findViewById(R.id.percentageOffET);
+        finalPriceTV = findViewById(R.id.finalPriceTV);
 
-                double originalPrice = Double.parseDouble(originalPriceET.getText().toString());
-                double discountPercentage = Double.parseDouble(discountPercentageET.getText().toString());
-                double finalPrice = originalPrice * (1 - (discountPercentage / 100.0));
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try {
+                    originalPrice = Double.parseDouble(originalPriceET.getText().toString());
+                    discountPercentage = Double.parseDouble(discountPercentageET.getText().toString());
+                }
+                catch (NumberFormatException e) {
+                    originalPrice = 0.0;
+                    discountPercentage = 0.0;
+                }
+
+                finalPrice = originalPrice * (1 - (discountPercentage / 100.0));
 
                 finalPriceTV.setText("Final Price : $ " + String.format("%.2f", finalPrice) + "");
             }
-        });
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+
+        discountPercentageET.addTextChangedListener(textWatcher);
+
+
+
+
     }
 
 }
