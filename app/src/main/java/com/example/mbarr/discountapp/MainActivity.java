46f81d24d6de4488e8fdbb612dc1b761bc73183a
mattheusbarroos <matheusbarros1305@gmail.com>
2018-03-15 00:56:52 -0400

    Calculating Price with percentage of discount

--- app/src/main/java/com/example/mbarr/discountapp/MainActivity.java
+++ app/src/main/java/com/example/mbarr/discountapp/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,38 +18,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar =  findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button calculateBtn =  findViewById(R.id.calculateBT);
+        calculateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                EditText originalPriceET =  findViewById(R.id.originalPriceET);
+                EditText discountPercentageET =  findViewById(R.id.percentageOffET);
+                TextView finalPriceTV =  findViewById(R.id.finalPriceTV);
+
+                double originalPrice = Double.parseDouble(originalPriceET.getText().toString());
+                double discountPercentage = Double.parseDouble(discountPercentageET.getText().toString());
+                double finalPrice = originalPrice * (1 - (discountPercentage / 100.0));
+
+                finalPriceTV.setText("Final Price : $ " + String.format("%.2f", finalPrice) + "");
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
